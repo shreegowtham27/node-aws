6a37cb0b47c7ddd76315203581ced15a28a5a009
shreegowtham27 <gforgowtham8@gmail.com>
2021-10-25 13:05:57 +0530

    Added Span tag for testing

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.listen(8081, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send('<html><head><title>sg.signiance.com</title></head><body><h1 align="center"> Hello World</h1></body></html>');
+  res.send('<html><head><title>sg.signiance.com</title></head><body><h1 align="center">Hello World</h1><span>Hello World 2</span></body></html>');
 });
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,5 @@ _#!/bin/bash_
 
 export PM2_HOME=/home/ubuntu/.pm2
 pm2 delete helloworld
-cd /var/www/html/helloworld
+cd /var/www/html/helloworld/
 pm2 start --name helloworld npm -- start
